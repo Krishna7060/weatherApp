82eb3af955664ad78b877d256c6f3b8aac5cb702
Krishna Nand <k.nautiyalkrishna70@gmail.com>
2024-07-15 11:21:34 +0530

    2nd commit

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
+import React from "react";
+import searchicon from "../assets/searchicon.png";
+import weatherimage from "../assets/weatherimage.png";
 
 function Weather() {
   return (
-    <div>
-        <h1 className='text-center text-black text-2xl'>Waether </h1>
-    </div>
+    <div className=" flex justify-center  min-h-screen min-w-screen bg-blue-500">
+      <div>
+        <div>
+          <h1 className="text-center font-bold">Today Weather</h1>
+        </div>
+        <div>
+          <div className="flex justify-center mt-2">
+            <input
+              type="text"
+              placeholder="Search City "
+              className="bg-gray-700 rounded-lg text-center font-bold"
+            />
+            <img src={searchicon} alt="search"  className="h-8 w-8 lg"/>
+          </div>
+          <div className="mt-3">
+            <img src={weatherimage} alt="" className="h-52 w-52 rounded-lg" />
+            <div className="font-bold ">
+                <p>City</p>
+            <img src="" alt="temp" /><span></span>
+            <img src="" alt="wind" /> <span></span>
+            <img src="" alt="pressure" /><span></span>
 
-  )
+            
+            
+            
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Weather
+export default Weather;
