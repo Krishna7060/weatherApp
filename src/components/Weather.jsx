b03800e600c5711d77a7b18f2f291fb0de9a560b
Krishna Nand <k.nautiyalkrishna70@gmail.com>
2024-07-15 09:57:35 +0530

    first commit

---
+++ src/components/Weather.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Weather() {
+  return (
+    <div>
+        <h1 className='text-center text-black text-2xl'>Waether </h1>
+    </div>
+
+  )
+}
+
+export default Weather
